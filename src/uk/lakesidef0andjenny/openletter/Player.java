2a2c4f05f57dfc102b4b3c6d5fb1bea749fd259c
Lewis Blackburn <blackbul@aston.ac.uk>
2016-12-20 12:32:56 +0000

    Added methods for player class

--- src/uk/lakesidef0andjenny/openletter/Player.java
+++ src/uk/lakesidef0andjenny/openletter/Player.java
@@ -1,5 +1,97 @@
 package uk.lakesidef0andjenny.openletter;
 
-public class Player {
+import uk.lakesidef0andjenny.openletter.cards.*;
 
+/**
+ * 
+ * Class responsible for containing player information
+ * @author Lewis Blackburn
+ * 
+ */
+public class Player {
+	// player's name
+	private String playerName;
+	// the card the player currently has in their hand
+	private Card currentCard;
+	// if the player can be targeted by the effects of cards
+	private boolean isProtected;
+	// if the player is currently in the game
+	private boolean isIn;
+	// how many rounds the player has won
+	private int score;
+
+	/**
+	 * constructor
+	 */
+	public Player(String playerName){
+		this.playerName = playerName;
+		currentCard = null;
+		isProtected = false;
+		isIn = true;
+		score = 0;
+	}
+
+	/**
+	 * accessor method for player's name
+	 * @return playerName
+	 */
+	public String getPlayerName(){
+		return playerName;
+	}
+
+	/**
+	 * accessor method for player's current card
+	 * @return currentCard
+	 */
+	public Card getCurrentCard(){
+		return currentCard;
+	}
+
+	/**
+	 * accessor method for if the player can be targeted
+	 * @return isProtected
+	 */
+	public boolean isProtected(){
+		return isProtected;
+	}
+
+	/**
+	 * accessor method for amount of points
+	 */
+	public int getScore(){
+		return score;
+	}
+
+	/**
+	 * accessor method for if the player is in the game
+	 */
+	public boolean isIn(){
+		return isIn;
+	}
+	
+	/**
+	 * changes if the player is in the game
+	 */
+	public void setIn(boolean isIn){
+		this.isIn = isIn;
+	}
+	
+	/**
+	 * String representation of player information
+	 */
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("Player name: ");
+		sb.append(playerName);
+		sb.append("\n");
+		if (isProtected){
+			sb.append("Cannot be targeted.");
+			sb.append("\n");	
+		}
+		sb.append("Won ");
+		sb.append(score);
+		sb.append(" rounds.");
+		return sb.toString();
+	}
 }
